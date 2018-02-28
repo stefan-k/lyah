b6e2538504fcc8e166bfbd9b0a2f4c60760aa261
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-28 14:43:59 +0100

    bla

--- 01.hs
+++ 01.hs
@@ -15,3 +15,26 @@ doubleSmallNumber' x =
   1
 
 lostNumbers = [4, 8, 15, 16, 23, 42]
+
+boomBangs xs =
+  [ if x < 10
+    then "Boom!"
+    else "Bang!"
+  | x <- xs
+  , odd x
+  ]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase :: String -> String
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
+
+removeOddNumbers xxs = [[x | x <- xs, even x] | xs <- xxs]
+
+rightTriangles x =
+  [ (a, b, c)
+  | c <- [1 .. x]
+  , b <- [1 .. c]
+  , a <- [1 .. b]
+  , a ^ 2 + b ^ 2 == c ^ 2
+  ]
