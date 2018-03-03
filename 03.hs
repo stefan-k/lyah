6b628ec01b683e1f9ced64d23bb34f63acfbf3eb
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-03 19:53:29 +0100

    higher order functions

--- 03.hs
+++ 03.hs
@@ -47,3 +47,52 @@ chain n
 
 countChains :: Int
 countChains = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
+
+sum' :: (Num a) => [a] -> a
+{- sum' xs = foldl (\acc x -> acc + x) 0 xs -}
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y = foldl (\acc x -> acc || (x == y)) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+mapl :: (a -> b) -> [a] -> [b]
+mapl f = foldl (\acc x -> acc ++ [f x]) []
+
+maximum' :: (Ord a) => [a] -> a
+maximum' =
+  foldr1
+    (\x acc ->
+       if x > acc
+         then x
+         else acc)
+
+reverse' :: [a] -> [a]
+{- reverse' = foldl (\acc x -> x : acc) [] -}
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f =
+  foldr
+    (\x acc ->
+       if f x
+         then x : acc
+         else acc)
+    []
+
+head' :: [a] -> a
+{- head' = foldr1 (\x _ -> x) -}
+head' = foldr1 const
+
+last' :: [a] -> a
+{- last' = foldl1 (\x _ -> x) -}
+last' = foldl1 const
+
+sqrtSums :: Int
+{- sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1 ..]))) + 1 -}
+sqrtSums = 1 + length (takeWhile (< 1000) $ scanl1 (+) $ map sqrt [1 ..])
