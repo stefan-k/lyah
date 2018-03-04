ff6c640c1244b4743f5bb747ff725c8808241afa
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-04 16:02:39 +0100

    more type stuff

---
+++ derive.hs
@@ -0,0 +1,65 @@
+import qualified Data.Map as Map
+
+data Person = Person
+  { firstName :: String
+  , lastName :: String
+  , age :: Int
+  } deriving (Eq, Show, Read)
+
+data Day
+  = Monday
+  | Tuesday
+  | Wednesday
+  | Thursday
+  | Friday
+  | Saturday
+  | Sunday
+  deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+type PhoneNumber = String
+
+type Name = String
+
+type PhoneBook = [(Name, PhoneNumber)]
+
+phoneBook :: PhoneBook
+phoneBook =
+  [ ("betty", "55555")
+  , ("bonnie", "44444")
+  , ("patsy", "33333")
+  , ("lucille", "222222")
+  , ("wendy", "11111")
+  , ("penny", "00000")
+  ]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+data LockerState
+  = Taken
+  | Free
+  deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockers :: LockerMap
+lockers =
+  Map.fromList
+    [ (100, (Taken, "ZD39I"))
+    , (101, (Free, "AAAA"))
+    , (103, (Free, "BBBB"))
+    , (105, (Free, "CCCC"))
+    , (109, (Taken, "DDDD"))
+    , (110, (Taken, "EEEE"))
+    ]
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+  case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesnt exist!"
+    Just (state, code) ->
+      if state /= Taken
+        then Right code
+        else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
