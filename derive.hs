b2667b9cf2eaa3e64ffdb406b6599716f673f960
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-04 16:26:45 +0100

    more types

--- derive.hs
+++ derive.hs
@@ -63,3 +63,17 @@ lockerLookup lockerNumber map =
       if state /= Taken
         then Right code
         else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+{- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord) -}
+infixr 5 :-:
+
+data List a
+  = Empty
+  | a :-: (List a)
+  deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
