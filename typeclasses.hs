64e89195b346dc2a2f294bd6027462348d182566
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-04 17:32:37 +0100

    typeclasses

---
+++ typeclasses.hs
@@ -0,0 +1,15 @@
+data TrafficLight
+  = Red
+  | Yellow
+  | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
