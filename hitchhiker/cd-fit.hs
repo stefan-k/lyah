f4930a54cf8fefabdda6663d7653b7cce5898bb0
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-06 21:07:27 +0100

    knapsack

--- hitchhiker/cd-fit.hs
+++ hitchhiker/cd-fit.hs
@@ -2,15 +2,14 @@ module Main where
 
 import Control.Monad (liftM2, replicateM)
 import qualified Data.Functor.Identity as DFI
-import Data.List (sortBy)
+import Data.Ix (inRange)
+import Data.List (maximumBy, notElem, sortBy)
 import Test.QuickCheck
 import qualified Text.Parsec.Prim as Prim
 import Text.ParserCombinators.Parsec
 
 -- we must teach QuickCheck how to generate arbitrary "Dir"s
-instance Arbitrary Dir
-  {- coarbitrary = undefined -}
-                                where
+instance Arbitrary Dir where
   arbitrary = liftM2 Dir genSize genName
     where
       genSize = do
@@ -27,6 +26,11 @@ prop_GreedyPackIsFixpoint ds =
   let pack = greedyPack ds
   in packSize pack == packSize (greedyPack (dirs pack))
 
+prop_DynamicPackIsFixpoint :: [Dir] -> Bool
+prop_DynamicPackIsFixpoint ds =
+  let pack = dynamicPack ds
+  in packSize pack == packSize (dynamicPack (dirs pack))
+
 -- parseInput parses output of "du -sb", which consists of many 
 -- lines, each of which describes a single directory
 parseInput :: Prim.ParsecT String () DFI.Identity [Dir]
@@ -38,16 +42,16 @@ parseInput = do
 -- Datatype Dir holds information about single directory 
 -- (size and name)
 data Dir = Dir
-  { dirSize :: Int
+  { dirSize :: Integer
   , dirName :: String
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 data DirPack = DirPack
-  { packSize :: Int
+  { packSize :: Integer
   , dirs :: [Dir]
   } deriving (Show)
 
-mediaSize :: Int
+mediaSize :: Integer
 mediaSize = 700 * 1024 * 1024
 
 dirAndSize :: Prim.ParsecT String u DFI.Identity Dir
@@ -69,6 +73,26 @@ maybeAddDir p d =
   in if newSize > mediaSize
        then p
        else DirPack newSize newDirs
+
+-- Dynamic programming solution to the knapsack problem
+precomputeDisksFor :: [Dir] -> [DirPack]
+precomputeDisksFor ddirs =
+  let precomp = map bestDisk [0 ..]
+      bestDisk 0 = DirPack 0 []
+      bestDisk limit =
+        case [ DirPack (dirSize d + s) (d : ds)
+             | d <- filter (inRange (1, limit) . dirSize) ddirs
+             , dirSize d > 0
+             , let (DirPack s ds) = precomp !! fromInteger (limit - dirSize d)
+             , d `notElem` ds
+             ] of
+          [] -> DirPack 0 []
+          packs -> maximumBy cmpSize packs
+      cmpSize a b = compare (packSize a) (packSize b)
+  in precomp
+
+dynamicPack :: [Dir] -> DirPack
+dynamicPack ddirs = precomputeDisksFor ddirs !! fromInteger mediaSize
 
 main :: IO ()
 main = do
