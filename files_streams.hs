51b6e9f5780e614c1c38adc9a305098bd1f3452e
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-09 08:47:03 +0100

    more files

--- files_streams.hs
+++ files_streams.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Control.Monad
 import Data.Char
+import Data.List
+import System.Directory
+import System.IO
 
 -- 1
 {- main :: IO () -}
@@ -39,28 +42,74 @@ import Data.Char
 {- main :: IO () -}
 {- main = interact $ unlines . filter ((< 10) . length) . lines -}
 -- 6
-main :: IO ()
-main = interact respondPalindrome
-
+{- main :: IO () -}
+{- main = interact respondPalindrome -}
+{-  -}
+{- {- respondPalindrome :: String -> String -} -}
+{- {- respondPalindrome contents = -} -}
+{- {-   unlines -} -}
+{- {-     (map -} -}
+{- {-        (\xs -> -} -}
+{- {-           if isPalindrome xs -} -}
+{- {-             then "palindrome" -} -}
+{- {-             else "not palindrome") -} -}
+{- {-        (lines contents)) -} -}
+{- {-   where -} -}
+{- {-     isPalindrome xs = xs == reverse xs -} -}
 {- respondPalindrome :: String -> String -}
-{- respondPalindrome contents = -}
-{-   unlines -}
-{-     (map -}
-{-        (\xs -> -}
-{-           if isPalindrome xs -}
-{-             then "palindrome" -}
-{-             else "not palindrome") -}
-{-        (lines contents)) -}
+{- respondPalindrome = -}
+{-   unlines . -}
+{-   map -}
+{-     (\xs -> -}
+{-        if isPalindrome xs -}
+{-          then "palindrome" -}
+{-          else "not palindrome") . -}
+{-   lines -}
 {-   where -}
 {-     isPalindrome xs = xs == reverse xs -}
-respondPalindrome :: String -> String
-respondPalindrome =
-  unlines .
-  map
-    (\xs ->
-       if isPalindrome xs
-         then "palindrome"
-         else "not palindrome") .
-  lines
-  where
-    isPalindrome xs = xs == reverse xs
+-- 7
+{- main :: IO () -}
+{- main = do -}
+{-   handle <- openFile "girlfriend.txt" ReadMode -}
+{-   contents <- hGetContents handle -}
+{-   putStr contents -}
+{-   hClose handle -}
+-- 8
+{- main :: IO () -}
+{- main = -}
+{-   withFile -}
+{-     "girlfriend.txt" -}
+{-     ReadMode -}
+{-     (\handle -> do -}
+{-        contents <- hGetContents handle -}
+{-        putStr contents) -}
+-- 9
+{- main :: IO () -}
+{- main = do -}
+{-   contents <- readFile "girlfriend.txt" -}
+{-   writeFile "girlfriendcaps.txt" $ map toUpper contents -}
+-- 10
+{- main :: IO () -}
+{- main = do -}
+{-   todoItem <- getLine -}
+{-   appendFile "todo.txt" $ todoItem ++ "\n" -}
+-- 11
+main :: IO ()
+main = do
+  handle <- openFile "todo.txt" ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  contents <- hGetContents handle
+  let todoTasks = lines contents
+      numberedTasks =
+        zipWith (\n line -> show n ++ " - " ++ line) [0 :: Integer ..] todoTasks
+  putStrLn "these are your todo items: "
+  putStr $ unlines numberedTasks
+  putStrLn "Which one do you want to delete?"
+  numberString <- getLine
+  let number = read numberString
+      newTodoItems = delete (todoTasks !! number) todoTasks
+  hPutStr tempHandle $ unlines newTodoItems
+  hClose handle
+  hClose tempHandle
+  removeFile "todo.txt"
+  renameFile tempName "todo.txt"
